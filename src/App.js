ea68e11a5e7b0f62ae38bcb5926c3ffc955af584
taisho <cunshangdajiang@gmail.com>
2021-05-01 07:43:44 +0900

    スネークを動かすまで

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React,{useState,useEffect} from 'react';
 
 import Navigation from './components/Navigation';
 import ManipulationPanel from './components/ManipulationPanel';
@@ -6,10 +6,49 @@ import Field from './components/Field';
 import Button from './components/Button';
 import {initFields} from './utils';
 
-const fields = initFields(35);
+const initialPosition = {x:17,y:17};
+const initialValues = initFields(35,initialPosition);
+const defaultInterval = 100;
 
+let timer = undefined;
+
+const unsubscribe = () => {
+  if(!timer){
+    return
+  }
+  clearInterval(timer)
+}
 
 function App() {
+
+  const [fields,setFields] = useState(initialValues);
+  const [position,setPosition] = useState();
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    setPosition(initialPosition)
+    timer = setInterval(() => {
+      setTick(tick => tick +1)
+    }, defaultInterval)
+    return unsubscribe
+  },[])
+
+  useEffect(() => {
+    if (!position) {
+      return
+    }
+    goUp()
+  }, [tick])
+
+  const goUp = () =>{
+    const {x,y} = position
+    const nextY = Math.max(y -1, 0)
+    fields[y][x] = ''
+    fields[nextY][x] = 'snake'
+    setPosition({ x, y: nextY })
+    setFields(fields)
+  }
+
   return (
     <div className="App">
 
